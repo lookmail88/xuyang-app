28f2b6f76c679e87b9e9af8f55a1742832669cb9
xuyang <lookmail88@gmail.com>
2026-03-29 11:15:46 -0700

    fix: test argo

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,7 @@ function App() {
             <li>
               <a href="https://vite.dev/" target="_blank">
                 <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
+                Explore Vite for testing argo
               </a>
             </li>
             <li>
